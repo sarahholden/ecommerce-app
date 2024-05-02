9addc52c9d0b591da6c55c1ab659e75055d42d3b
Sarah Holden <sarahbethholden@gmail.com>
2024-05-01 20:06:49 -0700

    Building out User models

--- models/user.js
+++ models/user.js
@@ -25,4 +25,71 @@ module.exports = class UserModel {
       throw new Error(err);
     }
   }
+
+  /**
+   * Updates a user record
+   * @param  {Object}      data [User data]
+   * @return {Object|null}      [Updated user record]
+   */
+  async update(data) {
+    try {
+      const { id, ...params } = data;
+
+      const condition = pgp.as.format("WHERE id = ${id} RETURNING *", { id });
+      const statement = pgp.helpers.update(params, null, "users") + condition;
+
+      const result = await db.query(statement);
+
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  /**
+   * Finds a user record by email
+   * @param  {String}      email [Email address]
+   * @return {Object|null}       [User record]
+   */
+  async findOneByEmail(email) {
+    try {
+      const statement = `SELECT * 
+                        FROM users
+                        WHERE email = $1`;
+      const values = [email];
+
+      const result = await db.query(statement, values);
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  /**
+   * Finds a user record by ID
+   * @param  {String}      id [User ID]
+   * @return {Object|null}    [User Record]
+   */
+  async findOneById(id) {
+    try {
+      const statement = `SELECT * 
+                        FROM users
+                        WHERE id = $1`;
+      const values = [id];
+
+      const result = db.query(statement, values);
+
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 };
