e8052d76962777ddd7ad4c628f02f5462f150d96
Sarah Holden <sarahbethholden@gmail.com>
2024-05-01 19:36:10 -0700

    Starting to set up Passport for auth

--- loaders/index.js
+++ loaders/index.js
@@ -1,11 +1,20 @@
 const expressLoader = require("./express");
-const routeLoader = require("../routes");
+const passportLoader = require("./passport");
+
+// const routeLoader = require("../routes");
 
 module.exports = async (app) => {
   const expressApp = await expressLoader(app);
 
   // TODO: load Passport Middleware
+  const passport = await passportLoader(expressApp);
 
   // Load API route handlers
-  await routeLoader(app, passport);
+  // await routeLoader(app, passport);
+
+  app.use((err, req, res, next) => {
+    const { message, status } = err;
+
+    return res.status(status).send({ message });
+  });
 };
---
+++ loaders/passport.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+module.exports = (app) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Set method to serialize data to store in cookie
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  // Set method to deserialize data stored in cookie and attach to req.user
+  passport.deserializeUser((id, done) => {
+    done(null, { id });
+  });
+
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        // TODO: GET USER
+        const user = null;
+        return done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    })
+  );
+
+  return passport;
+};
