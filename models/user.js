c521bbc9670b10816576df1ea8580b0fc0e59d38
Sarah Holden <sarahbethholden@gmail.com>
2024-05-01 19:58:09 -0700

    Logic for creating users

--- models/user.js
+++ models/user.js
@@ -1,3 +1,28 @@
 const db = require("../db");
 
-module.exports = class UserModel {};
+const pgp = require("pg-promise")({ capSQL: true });
+
+module.exports = class UserModel {
+  /**
+   * Creates a new user record
+   * @param  {Object}      data [User data]
+   * @return {Object|null}      [Created user record]
+   */
+  async create(data) {
+    try {
+      // Use pgp helper to dynamically inject params into SQL statement
+      const statement = pgp.helpers.insert(data, null, "users") + "RETURNING *";
+
+      // Execute
+      const result = await db.query(statement);
+
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+};
