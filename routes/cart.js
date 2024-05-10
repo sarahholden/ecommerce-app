9d6c5a5fb72ed85aed4dc56a1ecdb6b95063be52
Sarah Holden <sarahbethholden@gmail.com>
2024-05-10 12:03:08 -0700

    Updates for cart auth

--- routes/cart.js
+++ routes/cart.js
@@ -9,16 +9,65 @@ module.exports = (app) => {
   app.use("/carts", router);
 
   // Get /mine cart (load)
+  router.get("/mine", async (req, res, next) => {
+    try {
+      const { id } = req.user;
+    } catch (err) {
+      next(err);
+    }
+  });
 
   // PUT /mine
+  router.put("/mine", async (req, res, next) => {
+    try {
+      const { id } = req.user;
+    } catch (err) {
+      next(err);
+    }
+  });
 
   // POST /mine (create cart)
+  router.post("/mine", async (req, res, next) => {
+    try {
+      const { id } = req.user;
+
+      const response = await CartServiceInstance.create({ userId: id });
+
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
 
   // POST /mine/items (add item to cart)
+  router.post("/mine/items", async (req, res, next) => {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  });
 
   // PUT /mine/items/:cartItemId (update item in cart)
+  router.put("/mine/items/:cartItemId", async (req, res, next) => {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  });
 
   // DELETE /mine/items/:cartItemId
+  router.delete("/mine/items/:cartItemId", async (req, res, next) => {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  });
 
   // POST /mine/checkout
+  router.post("/mine/checkout", async (req, res, next) => {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  });
 };
--- services/CartService.js
+++ services/CartService.js
@@ -1,5 +1,20 @@
 const createError = require("http-errors");
 const CartModel = require("../models/cart");
-const CartModelInstance = new CartModel();
+const OrderModel = require("../models/order");
+const CartItemModel = require("../models/cartItem");
 
-module.exports = class CartService {};
+module.exports = class CartService {
+  async create(data) {
+    const { userId } = data;
+
+    try {
+      // Instantiate new cart and save
+      const Cart = new CartModel();
+      const cart = await Cart.create(userId);
+
+      return cart;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
